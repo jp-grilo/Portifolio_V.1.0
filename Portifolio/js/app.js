e078e7113729dcfd83c98c70bb3e5d84cec87903
João Paulo <58531784+jp-grilo@users.noreply.github.com>
2022-04-20 21:50:00 -0300

    Add files via upload

--- Portifolio/js/app.js
+++ Portifolio/js/app.js
@@ -1,31 +1,18 @@
 const header = document.querySelector("header");
 
-/* --------------- Grab elements from DOM --------------- */
-
-/* --------------- Sticky Navbar --------------- */
-function stickyNavbar() {
-    header.classList.toggle("scrolled", window.pageYOffset > 0);
-    //console.log(window.pageYOffset>0);
-}
-
-stickyNavbar();
-window.addEventListener("scroll", stickyNavbar);
-
-/* --------------- Reveal Animation --------------- */
-
-let sr = ScrollReveal({
-    duration: 2300,
-    distance: "60px"
-});
-
-sr.reveal(".showcase-info", { delay: 700 });
-sr.reveal(".showcase-image", { origin: "top", delay: 700 });
-
-/* --------------- Skills Progress Bar Animation --------------- */
-
 const first_skill = document.querySelector(".skill:first-child");
 const sk_counters = document.querySelectorAll(".counter span");
 const progress_bars = document.querySelectorAll(".skills svg circle");
+
+const ml_section = document.querySelector(".milestones");
+const ml_counters = document.querySelectorAll(".number span");
+
+window.addEventListener("scroll", stickyNavbar);
+
+window.addEventListener("scroll", () => {
+    if (!skillsPlayed) skillCounter();
+    if (!mlPlayed) mlCounter();
+})
 
 function hasReached(element) {
     let topPosition = element.getBoundingClientRect().top;
@@ -36,23 +23,61 @@ function hasReached(element) {
 
 function updateCount(num, maxNum) {
     let currentNum = +num.innerText;
-
     if (currentNum < maxNum) {
         num.innerText = currentNum + 1;
         setTimeout(() => {
             updateCount(num, maxNum);
         }, 12);
     }
-
 }
+
+function updateCountMl(num, maxNum) {
+
+    let currentNum = +num.innerText;
+    if (currentNum < maxNum) {
+
+        num.innerText = currentNum + 1;
+        setTimeout(() => {
+            updateCount(num, maxNum);
+        }, 75);
+
+    }
+}
+
+/* --------------- Grab elements from DOM --------------- */
+
+/* --------------- Sticky Navbar --------------- */
+function stickyNavbar() {
+    header.classList.toggle("scrolled", window.pageYOffset > 0);
+    //console.log(window.pageYOffset>0);
+}
+
+stickyNavbar();
+
+/* --------------- Reveal Animation --------------- */
+
+let sr = ScrollReveal({
+    duration: 1800,
+    distance: "60px"
+});
+
+sr.reveal(".showcase-info", { delay: 300 });
+sr.reveal(".showcase-image", { origin: "top", delay: 300 });
+
+/* --------------- Skills Progress Bar Animation --------------- */
+
+
+
+let skillsPlayed = false;
 
 function skillCounter() {
     if (!hasReached(first_skill)) return;
 
+    skillsPlayed = true;
+
     sk_counters.forEach((counter, i) => {
         let target = +counter.dataset.target;
         let strokeValue = 427 - 427 * (target / 100);
-        //console.log(strokeValue);
 
         progress_bars[i].style.setProperty("--target", strokeValue);
 
@@ -64,11 +89,22 @@ function skillCounter() {
     progress_bars.forEach((p) => (p.style.animation = "progress 2s ease-in-out forwards"));
 }
 
-window.addEventListener("scroll", () => {
-    skillCounter();
-})
-
 /* --------------- Services Counter Animation --------------- */
+
+let mlPlayed = false;
+
+function mlCounter() {
+    if (!hasReached(ml_section)) return;
+
+    mlPlayed = true;
+
+    ml_counters.forEach(ctr => {
+        let target = +ctr.dataset.target;
+        setTimeout(() => {
+            updateCountMl(ctr, target)
+        }, 400);
+    })
+}
 
 /* --------------- Portfolio Filter Animation --------------- */
 
